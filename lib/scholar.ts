fc14ff74b372428cf2544733de4690e26e8eed16
spencerwooo <spencer.woo@outlook.com>
2022-05-25 20:35:07 +0800

    add GOOGLE_SCHOLAR_COOKIE

--- lib/scholar.ts
+++ lib/scholar.ts
@@ -7,6 +7,7 @@ export const getPublications = async () => {
       headers: {
         'user-agent':
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36',
+        cookie: process.env.GOOGLE_SCHOLAR_COOKIE || '',
       },
     }
   )
