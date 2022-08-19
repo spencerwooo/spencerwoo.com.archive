74c2f99a3ac97598c5aa30914f007c3c1605190e
Spencer Woo <spencer.woo@outlook.com>
2022-08-19 16:21:14 +0800

    upgrade notion api to v2 (#9)

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,5 +1,21 @@
 import { Client } from '@notionhq/client'
-import { ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints'
+import type {
+  GetPagePropertyResponse,
+  ListBlockChildrenResponse,
+} from '@notionhq/client/build/src/api-endpoints'
+
+export type PageCompletePropertyResponse = {
+  id: string
+} & GetPagePropertyResponse
+export type PageCompletePropertyRecord = Record<
+  string,
+  PageCompletePropertyResponse
+>
+export type LatestPostProps = {
+  title: string
+  slug: string
+  emoji: string
+} | null
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId =
@@ -16,17 +32,80 @@ export const getDatabase = async (slug?: string) => {
     dbQuery.filter.and.push({ property: 'slug', rich_text: { equals: slug } })
   }
 
-  const response = await notion.databases.query(dbQuery)
-  return response.results
+  const { results } = await notion.databases.query(dbQuery)
+
+  // Each result (post) contains properties that should be extracted
+  // Props include - { published, tag, slug, author, date, preview, name }
+  await Promise.all(
+    results.map(async (res) => {
+      if ('properties' in res) {
+        for (const prop in res.properties) {
+          if (res.properties.hasOwnProperty(prop)) {
+            const propId = res.properties[prop].id
+            const propObj = await notion.pages.properties.retrieve({
+              page_id: res.id,
+              property_id: propId,
+            })
+
+            // Dumping every property into the result object as there is much
+            // to take care of (which will happen in React)
+            res.properties[prop] = { id: propId, ...propObj }
+          }
+        }
+      }
+
+      return res
+    })
+  )
+
+  return results
 }
 
-export const getLatestPost = async () => {
-  const response = await getDatabase()
-  return response[0]
+export const getLatestPostProps = async () => {
+  const { results } = await notion.databases.query({
+    database_id: databaseId,
+    filter: { and: [{ property: 'published', checkbox: { equals: true } }] },
+    sorts: [{ property: 'date', direction: 'descending' }],
+    page_size: 1,
+  })
+
+  const post = results[0]
+  if (!('icon' in post && 'properties' in post)) return null
+
+  const propExtractor = async (propId: string, pageId: string) => {
+    const prop = await notion.pages.properties.retrieve({
+      page_id: pageId,
+      property_id: propId,
+    })
+    if (!('results' in prop)) return ''
+    return prop.results.map((r: any) => r[r.type].plain_text).join('')
+  }
+
+  const emoji = post.icon?.type === 'emoji' ? post.icon.emoji : '🎑'
+  const slug = await propExtractor(post.properties.slug.id, post.id)
+  const title = await propExtractor(post.properties.name.id, post.id)
+  return {
+    emoji,
+    slug,
+    title,
+  } as LatestPostProps
 }
 
 export const getPage = async (pageId: string) => {
   const response = await notion.pages.retrieve({ page_id: pageId })
+
+  if ('properties' in response) {
+    for (const prop in response.properties) {
+      if (response.properties.hasOwnProperty(prop)) {
+        const propId = response.properties[prop].id
+        const propObj = await notion.pages.properties.retrieve({
+          page_id: response.id,
+          property_id: propId,
+        })
+        response.properties[prop] = { id: propId, ...propObj }
+      }
+    }
+  }
   return response
 }
 
