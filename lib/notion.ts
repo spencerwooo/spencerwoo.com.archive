9a8f9a46f2670962ff6d476e67bf3b10f3dac1b0
spencerwooo <spencer.woo@outlook.com>
2022-08-23 23:44:49 +0800

    catch rate limit error on latest post page

--- lib/notion.ts
+++ lib/notion.ts
@@ -71,33 +71,37 @@ export const getDatabase = async (slug?: string) => {
 }
 
 export const getLatestPostProps = async () => {
-  const { results } = await notion.databases.query({
-    database_id: databaseId,
-    filter: { and: [{ property: 'published', checkbox: { equals: true } }] },
-    sorts: [{ property: 'date', direction: 'descending' }],
-    page_size: 1,
-  })
-
-  const post = results[0]
-  if (!('icon' in post && 'properties' in post)) return null
-
-  const propExtractor = async (propId: string, pageId: string) => {
-    const prop = await notion.pages.properties.retrieve({
-      page_id: pageId,
-      property_id: propId,
+  try {
+    const { results } = await notion.databases.query({
+      database_id: databaseId,
+      filter: { and: [{ property: 'published', checkbox: { equals: true } }] },
+      sorts: [{ property: 'date', direction: 'descending' }],
+      page_size: 1,
     })
-    if (!('results' in prop)) return ''
-    return prop.results.map((r: any) => r[r.type].plain_text).join('')
-  }
 
-  const emoji = post.icon?.type === 'emoji' ? post.icon.emoji : '🎑'
-  const slug = await propExtractor(post.properties.slug.id, post.id)
-  const title = await propExtractor(post.properties.name.id, post.id)
-  return {
-    emoji,
-    slug,
-    title,
-  } as LatestPostProps
+    const post = results[0]
+    if (!('icon' in post && 'properties' in post)) return null
+
+    const propExtractor = async (propId: string, pageId: string) => {
+      const prop = await notion.pages.properties.retrieve({
+        page_id: pageId,
+        property_id: propId,
+      })
+      if (!('results' in prop)) return ''
+      return prop.results.map((r: any) => r[r.type].plain_text).join('')
+    }
+
+    const emoji = post.icon?.type === 'emoji' ? post.icon.emoji : '🎑'
+    const slug = await propExtractor(post.properties.slug.id, post.id)
+    const title = await propExtractor(post.properties.name.id, post.id)
+    return {
+      emoji,
+      slug,
+      title,
+    } as LatestPostProps
+  } catch (error) {
+    return null
+  }
 }
 
 export const getPage = async (pageId: string) => {
