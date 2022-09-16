9888ff2b7c5c14b47e341ff4ffc537583535267f
spencerwooo <spencer.woo@outlook.com>
2022-09-16 18:50:59 +0800

    static output instead of ssr

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
@@ -22,6 +22,6 @@ export default defineConfig({
     sitemap(),
     partytown({ config: { forward: ['dataLayer.push'] } }),
   ],
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
 })
